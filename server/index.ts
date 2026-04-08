3c3b7c33d40bd319363929777eb36046045ee4f4
Marcos Ramos <marcosramos.dev@gmail.com>
2026-04-08 12:18:33 -0300

    feat: refactor expenses route to use Hono and implement expense creation validation

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,9 @@ import { auth } from "./lib/auth";
 import { logger } from "hono/logger";
 import expensesRoute from "./routes/expenses.route";
 import type { Env } from "./types/Env.types";
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { createFiberplane } from "@fiberplane/hono";
+import { Hono } from "hono";
 
-const app = new OpenAPIHono<Env>();
+const app = new Hono<Env>();
 app.use(logger());
 
 const api = app.basePath("/api");
@@ -17,22 +16,5 @@ api.on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw));
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
-
-api.doc("/openapi.json", {
-  openapi: "3.0.0",
-  info: {
-    title: "Hono Expenses API",
-    version: "1.0.0",
-    description: "A simple API to manage expenses",
-  },
-});
-
-app.use(
-  "/fp/*",
-  createFiberplane({
-    app,
-    openapi: { url: "/api/openapi.json" },
-  }),
-);
 
 export default app;
--- server/routes/expenses.route.ts
+++ server/routes/expenses.route.ts
@@ -3,165 +3,44 @@ import {
   getExpensesQuery,
   insertExpenseQuery,
 } from "@/db/queries/expenses.query";
-import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
-import { expensesSchema } from "@/types/Expenses.types";
-import type { Env } from "@/types/Env.types";
+import { Hono } from "hono";
+import { createExpenseValidator } from "@/validators/expenses.validator";
 
-const messageSchema = z.object({
-  message: z.string(),
-});
+const expensesRoute = new Hono()
+  .use(authMiddleware)
+  .get("/", async (c) => {
+    const user = c.get("user");
+    if (!user) return c.json({ message: "You are not authenticated." }, 401);
+    try {
+      const expenses = await getExpensesQuery(user.id);
+      return c.json(expenses, 200);
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Error getting expenses." }, 500);
+    }
+  })
+  .post("/", createExpenseValidator, async (c) => {
+    const user = c.get("user");
+    const expenses = c.req.valid("json");
 
-const createExpenseSchema = z.object({
-  amount: z.string().min(1).max(100).openapi({
-    example: "100",
-    description: "The amount of the expense",
-  }),
-  description: z.string().max(300).optional().openapi({
-    example: "Lunch",
-    description: "A description of the expense",
-  }),
-}).openapi("CreateExpenseRequest");
+    if (!user) return c.json({ message: "You are not authenticated." }, 401);
 
-const createExpenseResponseSchema = z.object({
-  message: z.string().openapi({
-    example: "Expense created successfully.",
-  }),
-  newExpense: expensesSchema,
-}).openapi("CreateExpenseResponse");
-
-const getExpenses = createRoute({
-  method: "get",
-  path: "/",
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: expensesSchema.array(),
+    try {
+      const newExpense = await insertExpenseQuery({
+        ...expenses,
+        userId: user.id,
+      });
+      return c.json(
+        {
+          message: "Expense created successfully.",
+          newExpense,
         },
-      },
-      description: "Retrieve all expenses for the authenticated user",
-    },
-    401: {
-      content: {
-        "application/json": {
-          schema: messageSchema,
-          example: {
-            message: "You are not authenticated.",
-          },
-        },
-      },
-      description: "User is not authenticated",
-    },
-    500: {
-      content: {
-        "application/json": {
-          schema: messageSchema,
-          example: {
-            message: "Error getting expenses.",
-          },
-        },
-      },
-      description: "Error getting expenses",
-    },
-  },
-});
-
-const createExpense = createRoute({
-  method: "post",
-  path: "/",
-  request: {
-    body: {
-      content: {
-        "application/json": {
-          schema: createExpenseSchema,
-        },
-      },
-      required: true,
-    },
-  },
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: createExpenseResponseSchema,
-        },
-      },
-      description: "Expense created successfully",
-    },
-    400: {
-      content: {
-        "application/json": {
-          schema: z.array(z.any()),
-          example: [
-            ["amount", "String must contain at least 1 character(s)"],
-          ],
-        },
-      },
-      description: "Validation error",
-    },
-    401: {
-      content: {
-        "application/json": {
-          schema: messageSchema,
-          example: {
-            message: "You are not authenticated.",
-          },
-        },
-      },
-      description: "User is not authenticated",
-    },
-    500: {
-      content: {
-        "application/json": {
-          schema: messageSchema,
-          example: {
-            message: "Error creating expense.",
-          },
-        },
-      },
-      description: "Error creating expense",
-    },
-  },
-});
-
-const expensesRoute = new OpenAPIHono<Env>();
-
-expensesRoute.use(authMiddleware);
-
-expensesRoute.openapi(getExpenses, async (c) => {
-  const user = c.get("user");
-  if (!user) return c.json({ message: "You are not authenticated." }, 401);
-  try {
-    const expenses = await getExpensesQuery(user.id);
-    return c.json(expenses, 200);
-  } catch (error) {
-    console.error(error);
-    return c.json({ message: "Error getting expenses." }, 500);
-  }
-});
-
-expensesRoute.openapi(createExpense, async (c) => {
-  const user = c.get("user");
-  const expenses = c.req.valid("json");
-
-  if (!user) return c.json({ message: "You are not authenticated." }, 401);
-
-  try {
-    const newExpense = await insertExpenseQuery({
-      ...expenses,
-      userId: user.id,
-    });
-    return c.json(
-      {
-        message: "Expense created successfully.",
-        newExpense,
-      },
-      200,
-    );
-  } catch (error) {
-    console.error(error);
-    return c.json({ message: "Error creating expense." }, 500);
-  }
-});
+        200,
+      );
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Error creating expense." }, 500);
+    }
+  });
 
 export default expensesRoute;
